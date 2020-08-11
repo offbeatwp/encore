21056b4a20d4f0093eec56db596a2ce1f4af89e8
Chris Schrijver <chris@raow.nl>
2020-08-11 09:03:53 +0200

    fix for icons

--- src/icons.js
+++ src/icons.js
@@ -38,7 +38,7 @@ module.exports = function () {
         cssDest:        path.join(outputDirectory, '_' + fontName + '.scss'),
         html:           false,
         cssTemplate:    path.resolve(__dirname, '../config/icons-css.hbs'),
-        cssFontsUrl:   'fonts/' + fontName + '/',
+        cssFontsUrl:   '',
         fontName:       fontName,
         templateOptions: {
             classPrefix:    fontName + '-',
