ec90c0c7585de430052e83032ba6e7e2079c9581
Chris Schrijver <chris@raow.nl>
2022-08-08 23:12:21 +0200

    prep replace web font generator

--- src/icons.js
+++ src/icons.js
@@ -1,54 +1,30 @@
-const webfontsGenerator = require('webfonts-generator');
-const glob = require('glob');
-const path = require('path');
-const fs = require('fs');
-
-function getPaths(globs) {
-    let out = [];
-
-    globs.forEach(str => out = out.concat(glob.sync(str)));
-
-    return out;
-}
-
-function ensureDirectoryExistence(dirname) {
-    if (fs.existsSync(dirname)) {
-        return true;
-    }
-
-    const parentDirName = path.dirname(dirname);
-    ensureDirectoryExistence(parentDirName);
-
-    fs.mkdirSync(dirname);
-    return true;
-}
 
 module.exports = function () {
-    const baseDirectory = process.cwd();
+    // const baseDirectory = process.cwd();
 
-    const outputDirectory = path.resolve(baseDirectory, 'assets/fonts/icons');
-    ensureDirectoryExistence(outputDirectory);
+    // const outputDirectory = path.resolve(baseDirectory, 'assets/fonts/icons');
+    // ensureDirectoryExistence(outputDirectory);
 
-    const files = getPaths([path.resolve(baseDirectory, 'assets/icons/*.svg')]);
-    const fontName = 'oif';
+    // const files = getPaths([path.resolve(baseDirectory, 'assets/icons/*.svg')]);
+    // const fontName = 'oif';
     
-    webfontsGenerator({
-        files:          files,
-        dest:           outputDirectory,
-        cssDest:        path.join(outputDirectory, '_' + fontName + '.scss'),
-        html:           false,
-        cssTemplate:    path.resolve(__dirname, '../config/icons-css.hbs'),
-        cssFontsUrl:   '',
-        fontName:       fontName,
-        templateOptions: {
-            classPrefix:    fontName + '-',
-        }
-    }, function(error) {
-        if (error) {
-            console.error('Fail!', error);
-        } else {
-            console.log('Done!');
-        }
-    });
+    // webfontsGenerator({
+    //     files:          files,
+    //     dest:           outputDirectory,
+    //     cssDest:        path.join(outputDirectory, '_' + fontName + '.scss'),
+    //     html:           false,
+    //     cssTemplate:    path.resolve(__dirname, '../config/icons-css.hbs'),
+    //     cssFontsUrl:   '',
+    //     fontName:       fontName,
+    //     templateOptions: {
+    //         classPrefix:    fontName + '-',
+    //     }
+    // }, function(error) {
+    //     if (error) {
+    //         console.error('Fail!', error);
+    //     } else {
+    //         console.log('Done!');
+    //     }
+    // });
     
 };
